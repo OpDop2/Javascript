01b3cfcd5afff5de46aeeb7b9b0c8c96cbedf6c3
OpDop2 <testingop2236@gmail.com>
2025-01-01 03:17:52 +0000

    timepass

---
+++ 01basics/String.js
@@ -0,0 +1,20 @@
+const gameName = new String("Empty")
+const timepass = new String("hc-hjtesh-f")
+const triming = "   jjj     "
+console.log(triming)
+console.log(triming.trim())
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice(-4.4)
+console.log(anotherString)
+//For timepass
+console.log(gameName.toLowerCase())
+console.log(gameName.toUpperCase())
+//replacing
+const url = "great"
+console.log(url.replace("a", "b"))
+//Time pass
+console.log(gameName.includes("gf"));
+
