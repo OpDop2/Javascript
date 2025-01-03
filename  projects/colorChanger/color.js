938ed806bc1ce0b969210ff78aaa2af303fbcd2a
OpDop2 <testingop2236@gmail.com>
2025-01-03 06:11:37 +0000

    hhh

---  projects/colorChanger/color.js
+++  projects/colorChanger/color.js
@@ -1,3 +1,16 @@
 const button  = document.querySelectorAll('.button')
 const body = document.querySelector("body")
 
+button.forEach(function(button) {
+   button.addEventListener('click', function(event) {
+   console.log(event.target)
+   if(event.target.id === 'grey'){
+      body.style.backgroundColor = event.target.id
+   }  
+   if(event.target.id === 'yellow'){
+      body.style.backgroundColor = event.target.id
+   } 
+   if(event.target.id === 'blue'){
+      body.style.backgroundColor = event.target.id
+   }
+})})
