f869a2a89fb65d8f9eae4edfc18285d8a44f343e
OpDop2 <testingop2236@gmail.com>
2025-01-18 07:29:19 +0000

    git push

---
+++ 10_classes_and_oops /objetcs.js
@@ -0,0 +1,8 @@
+function mutliplyby5(num) {
+   return num * 5
+}
+mutliplyby5.power = 5
+console.log(mutliplyby5(5))
+console.log(mutliplyby5.power)
+console.log(mutliplyby5.prototype)
+// function is an object
